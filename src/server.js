1484474dec58831a968c3ff3496cd1a5d70ae673
L8ab <gpf102030@gmail.com>
2025-12-10 01:26:22 +0300

    Add src/server.js

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require('apollo-server');
+const { typeDefs, resolvers } = require('./schema');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
+
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`GraphQL server ready at ${url}`);
+});
